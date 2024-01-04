f6ca55164382d53c588c11682be2878685a5219f
sravani <konerusravani99@gmail.com>
2024-01-04 15:09:17 +0530

    navbar component generated

---
+++ src/app/navbar-1/navbar-1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-1',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar-1.component.html',
+  styleUrl: './navbar-1.component.css'
+})
+export class Navbar1Component {
+
+}
